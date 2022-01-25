e3f131407955b414a37fa9b5c1ad5ac59cc2dc94
dyau <dennisyau@gmail.com>
2022-01-25 12:58:32 +0000

    add non-zero exit codes

--- anagram.sh
+++ anagram.sh
@@ -4,13 +4,13 @@
 # if they are anagrams of each other return true
 # else return false 
 
-[[ $# != 2 ]] && echo "$0 word1 word2" && exit 0
+[[ $# != 2 ]] && echo "$0 word1 word2" && exit 2
 
-[[ "${#1}" != "${#2}" ]] && echo "false" && exit 0
+[[ "${#1}" != "${#2}" ]] && echo "false" && exit 1
 
 WORD1=$(echo "$1" |grep -o . |sort |tr -d '\n')
 WORD2=$(echo "$2" |grep -o . |sort |tr -d '\n')
 
-[[ "$WORD1" != "$WORD2" ]] && echo "false" && exit 0
+[[ "$WORD1" != "$WORD2" ]] && echo "false" && exit 1
 
 echo "true"
